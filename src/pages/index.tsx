4ddefa0b54284513aa13ffe071fffe64ce793cc9
kaisiq <dahudohu@gmail.com>
2023-05-17 13:23:29 +0300

    add adsense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
         <title>UnDisturbed</title>
         <meta name="description" content="UnDisturbed Minecraft server" />
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4556884557249419" crossOrigin="anonymous"></script>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
